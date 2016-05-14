00f9ee79c89dacc0d47b4b740285a680ebd97432
jordiroig <jordi@jordiroig.com>
2016-05-14 13:59:16 +0200

    Member entity

--- src/Atrapalo/InheritancesBundle/Entity/Member.php
+++ src/Atrapalo/InheritancesBundle/Entity/Member.php
@@ -2,6 +2,9 @@
 
 namespace Atrapalo\InheritancesBundle\Entity;
 
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -22,7 +25,7 @@ class Member
     private $id;
 
     /**
-     * @var \DateTime
+     * @var DateTime
      *
      * @ORM\Column(name="birthdate", type="date")
      */
@@ -35,6 +38,49 @@ class Member
      */
     private $name;
 
+    /**
+     * @var Member
+     *
+     * @ORM\ManyToOne(targetEntity="Atrapalo\InheritancesBundle\Entity\Member", inversedBy="sons")
+     * @ORM\JoinColumn(name="father", referencedColumnName="id", nullable=true)
+     */
+    private $father;
+
+    /**
+     * @var Collection
+     *
+     * @ORM\OneToMany(targetEntity="Atrapalo\InheritancesBundle\Entity\Member", mappedBy="father", cascade={"remove", "persist"}, orphanRemoval=true)
+     */
+    private $sons;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="lands", type="integer", nullable=false, options={"default":0})
+     */
+    private $lands = 0;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="money", type="decimal", precision=10, scale=0, nullable=false, options={"default":0})
+     */
+    private $money = 0;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="properties", type="integer", nullable=false, options={"default":0})
+     */
+    private $properties = 0;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->sons = new ArrayCollection();
+    }
 
     /**
      * Get id
@@ -49,7 +95,7 @@ class Member
     /**
      * Set birthdate
      *
-     * @param \DateTime $birthdate
+     * @param DateTime $birthdate
      * @return Member
      */
     public function setBirthdate($birthdate)
@@ -62,7 +108,7 @@ class Member
     /**
      * Get birthdate
      *
-     * @return \DateTime 
+     * @return DateTime
      */
     public function getBirthdate()
     {
@@ -90,5 +136,134 @@ class Member
     public function getName()
     {
         return $this->name;
+    }
+
+    /**
+     * Set father
+     *
+     * @param Member $father
+     * @return Member
+     */
+    public function setFather(Member $father)
+    {
+        $this->father = $father;
+        return $this;
+    }
+    /**
+     * Get father
+     *
+     * @return Member
+     */
+    public function getFather()
+    {
+        return $this->father;
+    }
+
+    /**
+     * Set sons
+     *
+     * @param Collection $sons
+     * @return Member
+     */
+    public function setSons(Collection $sons)
+    {
+        if (count($sons) > 0) foreach ($sons as $son) $this->addSon($son);
+        return $this;
+    }
+    /**
+     * Add son
+     *
+     * @param Member $son
+     * @return Member
+     */
+    public function addSon(Member $son)
+    {
+        $son->setFather($this);
+        $this->getSons()->add($son);
+        return $this;
+    }
+    /**
+     * Remove son
+     *
+     * @param Member $son
+     */
+    public function removeSon(Member $son)
+    {
+        $this->sons->removeElement($son);
+    }
+    /**
+     * Get sons
+     *
+     * @return Collection
+     */
+    public function getSons()
+    {
+        return $this->sons;
+    }
+
+    /**
+     * Get lands
+     *
+     * @return int
+     */
+    public function getLands()
+    {
+        return $this->lands;
+    }
+
+    /**
+     * Set lands
+     *
+     * @param int $lands
+     * @return Member
+     */
+    public function setLands($lands)
+    {
+        $this->lands = $lands;
+        return $this;
+    }
+
+    /**
+     * Get money
+     *
+     * @return float
+     */
+    public function getMoney()
+    {
+        return $this->money;
+    }
+
+    /**
+     * Set money
+     *
+     * @param float $money
+     * @return $this
+     */
+    public function setMoney($money)
+    {
+        $this->money = $money;
+        return $this;
+    }
+
+    /**
+     * Get properties
+     *
+     * @return int
+     */
+    public function getProperties()
+    {
+        return $this->properties;
+    }
+
+    /**
+     * Set properties
+     *
+     * @param int $properties
+     * @return $this
+     */
+    public function setProperties($properties)
+    {
+        $this->properties = $properties;
+        return $this;
     }
 }
