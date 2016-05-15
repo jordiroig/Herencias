5044ba5c0f11be71e4f760e817fe1a3863a077b1
jordiroig <jordi@jordiroig.com>
2016-05-15 18:29:00 +0200

    Accountant service

--- src/Atrapalo/InheritancesBundle/Controller/DefaultController.php
+++ src/Atrapalo/InheritancesBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Atrapalo\InheritancesBundle\Controller;
 
 use Atrapalo\InheritancesBundle\Entity\Member;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -14,17 +15,17 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $manel = $this->getDoctrine()->getRepository('AtrapaloInheritancesBundle:Member')->find(1);
-        $this->get('atrapalo.inheritances.notary.distributor')->distributeInheritance($manel, 20 , 1000, 8);
-
+        $moment = DateTime::createFromFormat('d-m-Y', '01-01-2025');
+        $this->get('atrapalo.inheritances.notary.accountant')->updateFamilyStatusByDate($manel, $moment);
 
         $sons = $manel->getSons();
 
         /** @var Member $son */
         foreach ($sons as $son) {
-            echo $son->getName()." - ".$son->getInheritanceMoney()." - ".$son->getInheritanceLands()." - ".$son->getInheritanceProperties()."<br /><br />";
+            echo $son->getName()." - ".($son->isDead($moment))?"mort":"viu"." - ".$son->getInheritanceMoney()." - ".$son->getInheritanceLands()." - ".$son->getInheritanceProperties()."<br /><br />";
             if($grandsons = $son->getSons()) {
                 foreach ($grandsons as $grandson) {
-                    echo "&nbsp;&nbsp;&nbsp;".$grandson->getName()." - ".$grandson->getInheritanceMoney()." - ".$grandson->getInheritanceLands()." - ".$grandson->getInheritanceProperties()."<br /><br />";
+                    echo "&nbsp;&nbsp;&nbsp;".$grandson->getName()." - ".($son->isDead($moment))?"mort":"viu"." - ".$grandson->getInheritanceMoney()." - ".$grandson->getInheritanceLands()." - ".$grandson->getInheritanceProperties()."<br /><br />";
                 }
             }
         }
--- src/Atrapalo/InheritancesBundle/Services/Notary/Accountant.php
+++ src/Atrapalo/InheritancesBundle/Services/Notary/Accountant.php
@@ -2,6 +2,9 @@
 
 namespace Atrapalo\InheritancesBundle\Services\Notary;
 
+use Atrapalo\InheritancesBundle\Entity\Member;
+use DateTime;
+
 class Accountant
 {
     private $distributor;
@@ -17,6 +20,28 @@ class Accountant
         $this->distributor = $distributor;
     }
 
-    
+    /**
+     * Update family status
+     *
+     * @param Member $member
+     * @param DateTime $moment
+     * @return Member
+     */
+    public function updateFamilyStatusByDate(Member $member, DateTime $moment)
+    {
+        if($member->isDead($moment) && $sons = $member->getSons()) {
+            $lands = $member->getLands() + $member->getInheritanceLands();
+            $money = $member->getMoney() + $member->getInheritanceMoney();
+            $properties = $member->getProperties() + $member->getInheritanceProperties();
+
+            $this->distributor->distributeInheritance($member, $lands, $money, $properties);
+
+            foreach($sons as $son) {
+                $this->updateFamilyStatusByDate($son, $moment);
+            }
+        }
+
+        return $member;
+    }
 
 }
