9ecbc74f4de1c6d37d7f3b6aa1e0dd1ba4255c00
jordiroig <jordi@jordiroig.com>
2016-05-15 14:00:27 +0200

    Bundles required

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Atrapalo\InheritancesBundle\AtrapaloInheritancesBundle(),
             new FOS\RestBundle\FOSRestBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
