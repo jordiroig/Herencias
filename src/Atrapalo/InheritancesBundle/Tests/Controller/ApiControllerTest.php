3a5ae05d966afdff75e8a259510e5cd607436e1a
jordiroig <jordi@jordiroig.com>
2016-05-16 16:41:33 +0200

    PHPUnit testing

---
+++ src/Atrapalo/InheritancesBundle/Tests/Controller/ApiControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Atrapalo\InheritancesBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ApiControllerTest extends WebTestCase
+{
+    public function testGetTweetsAction()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/v1/heritage/julia/01-01-2075');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        die($crawler->html());
+    }
+
+}
