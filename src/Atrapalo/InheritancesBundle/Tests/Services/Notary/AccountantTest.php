4aa3cad037bbac2f4b1294e2d2f6b8fb26378df9
jordiroig <jordi@jordiroig.com>
2016-05-16 15:07:09 +0200

    PHPUnit testing

--- src/Atrapalo/InheritancesBundle/Tests/Services/Notary/AccountantTest.php
+++ src/Atrapalo/InheritancesBundle/Tests/Services/Notary/AccountantTest.php
@@ -5,11 +5,50 @@ namespace Atrapalo\InheritancesBundle\Tests\Services\Notary;
 use Atrapalo\InheritancesBundle\Entity\Member;
 use Atrapalo\InheritancesBundle\Services\Notary\Accountant;
 use Atrapalo\InheritancesBundle\Services\Notary\Distributor;
+use DateTime;
 use Doctrine\Common\Collections\ArrayCollection;
 use PHPUnit_Framework_TestCase;
 
 class AccountantTest extends PHPUnit_Framework_TestCase
 {
+    public function testUpdateFamilyStatusByDate()
+    {
+        $distributor = $this->getMockBuilder('Atrapalo\InheritancesBundle\Services\Notary\Distributor')->disableOriginalConstructor()->getMock();
+        $distributor->expects($this->exactly(2))
+            ->method('distributeInheritance');
+        /** @var Distributor $distributor */
+        $accountant = new Accountant($distributor);
+
+        $father = new Member();
+        $father ->setName('father')
+            ->setBirthdate(DateTime::createFromFormat('d-m-Y', '01-01-1975'))
+            ->setLands(2)
+            ->setMoney(1000)
+            ->setProperties(4);
+
+        $son1 = new Member();
+        $son1->setName('son1')
+            ->setFather($father)
+            ->setBirthdate(DateTime::createFromFormat('d-m-Y', '01-01-1995'));
+        $son2 = new Member();
+        $son2->setName('son2')
+            ->setFather($father)
+            ->setBirthdate(DateTime::createFromFormat('d-m-Y', '01-01-1996'));
+
+        $sons = new ArrayCollection(array($son1, $son2));
+        $father->setSons($sons);
+
+        $grandson1 = new Member();
+        $grandson1->setName('grandson1')
+            ->setFather($son1)
+            ->setBirthdate(DateTime::createFromFormat('d-m-Y', '01-01-2015'));
+
+        $grandsons = new ArrayCollection(array($grandson1));
+        $son1->setSons($grandsons);
+
+        $accountant->updateFamilyStatusByDate($father, DateTime::createFromFormat('d-m-Y', '01-01-2110'));
+    }
+
     public function testGetTotalHeritageByMember()
     {
         $distributor = $this->getMockBuilder('Atrapalo\InheritancesBundle\Services\Notary\Distributor')->disableOriginalConstructor()->getMock();
