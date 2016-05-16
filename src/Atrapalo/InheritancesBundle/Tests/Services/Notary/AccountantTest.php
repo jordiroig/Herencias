98e7221a6fa7b17d502dcd59aeb051811f135283
jordiroig <jordi@jordiroig.com>
2016-05-16 16:17:13 +0200

    PHPUnit testing

--- src/Atrapalo/InheritancesBundle/Tests/Services/Notary/AccountantTest.php
+++ src/Atrapalo/InheritancesBundle/Tests/Services/Notary/AccountantTest.php
@@ -119,4 +119,48 @@ class AccountantTest extends PHPUnit_Framework_TestCase
         $expected = new ArrayCollection(array($son, $father, $grandfather));
         $this->assertEquals($expected, $result['branch']);
     }
+
+    public function testGetTotalHeritageByMemberAndDate()
+    {
+        $father = new Member();
+        $father ->setName('father')
+            ->setBirthdate(DateTime::createFromFormat('d-m-Y', '01-01-1975'))
+            ->setLands(2)
+            ->setMoney(1000)
+            ->setProperties(4);
+
+        $son1 = new Member();
+        $son1->setName('son1')
+            ->setFather($father)
+            ->setBirthdate(DateTime::createFromFormat('d-m-Y', '01-01-1995'));
+        $son2 = new Member();
+        $son2->setName('son2')
+            ->setFather($father)
+            ->setBirthdate(DateTime::createFromFormat('d-m-Y', '01-01-1998'));
+
+        $sons = new ArrayCollection(array($son1, $son2));
+        $father->setSons($sons);
+
+        $grandson1 = new Member();
+        $grandson1->setName('grandson1')
+            ->setFather($son1)
+            ->setBirthdate(DateTime::createFromFormat('d-m-Y', '01-01-2015'));
+
+        $son1->addSon($grandson1);
+
+        $grandson2 = new Member();
+        $grandson2->setName('grandson2')
+            ->setFather($son2)
+            ->setBirthdate(DateTime::createFromFormat('d-m-Y', '01-01-2014'));
+
+        $son2->addSon($grandson2);
+
+        $distributor = $this->getMockBuilder('Atrapalo\InheritancesBundle\Services\Notary\Distributor')->disableOriginalConstructor()->getMock();
+        /** @var Distributor $distributor */
+        $accountant = new Accountant($distributor);
+
+        $result = $accountant->getTotalHeritageByMemberAndDate($son1, DateTime::createFromFormat('d-m-Y', '01-01-2096'));
+
+        $this->assertEquals(2001100, $result);
+    }
 }
