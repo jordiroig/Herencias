771fe0857770e9d6eab3020978088759555fa7bb
jordiroig <jordi@jordiroig.com>
2016-05-16 15:55:33 +0200

    PHPUnit testing

--- src/Atrapalo/InheritancesBundle/Tests/Services/Notary/AccountantTest.php
+++ src/Atrapalo/InheritancesBundle/Tests/Services/Notary/AccountantTest.php
@@ -14,7 +14,8 @@ class AccountantTest extends PHPUnit_Framework_TestCase
     public function testUpdateFamilyStatusByDate()
     {
         $distributor = $this->getMockBuilder('Atrapalo\InheritancesBundle\Services\Notary\Distributor')->disableOriginalConstructor()->getMock();
-        $distributor->expects($this->exactly(1))
+        $distributor
+            ->expects($this->exactly(2))
             ->method('distributeInheritance');
         /** @var Distributor $distributor */
         $accountant = new Accountant($distributor);
